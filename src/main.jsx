7c7939f222005c3e19675a9fcf4d1d9a7536a034
p1ck234 <danil-vetrov47@mail.ru>
2024-09-23 00:22:58 +0400

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
-import { render } from "preact";
-import App from "./app.jsx";
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
 import "./index.css";
 
-render(<App />, document.getElementById("app"));
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
