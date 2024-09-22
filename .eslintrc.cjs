d82733082edcaed5ac404d62f44fec663d013164
p1ck234 <danil-vetrov47@mail.ru>
2024-09-23 01:42:03 +0400

    setting eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'off',
+  },
+};
