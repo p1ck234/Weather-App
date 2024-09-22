4840832e07d033a88d77e4da36f8dbd93610313b
p1ck234 <danil-vetrov47@mail.ru>
2024-09-23 01:42:59 +0400

    setting jest

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.[tj]sx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+  setupFiles: ["<rootDir>/jest.env.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+};
