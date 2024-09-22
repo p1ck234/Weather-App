c033fa632b797b9dc7dff2e5cdacde9d787fa9cf
p1ck234 <danil-vetrov47@mail.ru>
2024-09-23 01:43:55 +0400

    setting app

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,33 @@
-// src/App.jsx
-import React from "react";
-import axios from "axios";
-import Search from "./components/Search/Search";
-import WeatherDetails from "./components/WeatherDetails/WeatherDetails";
-import Forecast from "./components/Forecast/Forecast";
-import "./index.css"; // Не забудьте импортировать стили
+import React from 'react';
+import axios from 'axios';
+import Search from './components/Search/Search';
+import WeatherDetails from './components/WeatherDetails/WeatherDetails';
+import Forecast from './components/Forecast/Forecast';
+import './index.css';
 
 function App() {
   const [weather, setWeather] = React.useState(null);
   const [forecast, setForecast] = React.useState(null);
-  const [error, setError] = React.useState("");
+  const [error, setError] = React.useState('');
   const [loading, setLoading] = React.useState(false);
 
-  const apiKey = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
+  var apiKey = process.env.VITE_OPENWEATHERMAP_API_KEY;
 
   const handleSearch = async (city) => {
     try {
-      setError("");
+      setError('');
       setLoading(true);
-      // Запрос текущей погоды
       const weatherResponse = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`,
       );
       setWeather(weatherResponse.data);
 
-      // Запрос прогноза на 5 дней
       const forecastResponse = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=ru`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=ru`,
       );
       setForecast(forecastResponse.data);
-    } catch (err) {
-      setError("Город не найден или произошла ошибка при запросе.");
+    } catch {
+      setError('Город не найден или произошла ошибка при запросе.');
       setWeather(null);
       setForecast(null);
     } finally {
@@ -42,9 +39,9 @@ function App() {
     <div>
       <h1>Прогноз погоды</h1>
       <Search onSearch={handleSearch} />
-      {error && <p className="error-message">{error}</p>}
+      {error && <p className='error-message'>{error}</p>}
       {loading ? (
-        <div className="loader"></div>
+        <div data-testid='loader' className='loader'></div>
       ) : (
         <>
           <WeatherDetails weather={weather} />
