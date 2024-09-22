ae5ed237d0183c5c9acec344e994ebfe6e5bd2f7
p1ck234 <danil-vetrov47@mail.ru>
2024-09-23 00:22:48 +0400

    setting forecast

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,29 +1,40 @@
-// src/components/Forecast.jsx
+// src/components/Forecast/Forecast.jsx
 import React from "react";
 
 function Forecast({ forecast }) {
   if (!forecast) return null;
 
-  // Группируем прогноз по дням
-  const dailyForecast = forecast.list.filter((item) =>
-    item.dt_txt.includes("12:00:00")
-  );
+  // Получаем прогноз на полдень каждого дня
+  const dailyForecast = [];
+  const dates = new Set();
+
+  for (const item of forecast.list) {
+    const date = item.dt_txt.split(" ")[0];
+    if (!dates.has(date) && item.dt_txt.includes("12:00:00")) {
+      dates.add(date);
+      dailyForecast.push(item);
+    }
+    if (dailyForecast.length === 5) break;
+  }
 
   return (
-    <div>
+    <div className="forecast-container">
       <h2>Прогноз на 5 дней</h2>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        {dailyForecast.map((item) => (
-          <div key={item.dt}>
-            <p>{new Date(item.dt_txt).toLocaleDateString()}</p>
-            <p>Температура: {item.main.temp} °C</p>
-            <p>{item.weather[0].description}</p>
-            <img
-              src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
-              alt={item.weather[0].description}
-            />
-          </div>
-        ))}
+      <div className="forecast-cards">
+        {dailyForecast.map((item) => {
+          const weatherIcon = item.weather[0].icon.replace("n", "d");
+          const weatherDescription = item.weather[0].description;
+          const iconUrl = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+
+          return (
+            <div className="forecast-card" key={item.dt}>
+              <p>{new Date(item.dt_txt).toLocaleDateString()}</p>
+              {weatherIcon && <img src={iconUrl} alt={weatherDescription} />}
+              <p>{weatherDescription}</p>
+              <p>Температура: {Math.round(item.main.temp)} °C</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
